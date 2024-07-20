155ff608ddce0d34deae652317bc040d77928c23
Cassiano D'Andrea <cassiano.dandrea@gmail.com>
2024-07-20 10:39:02 -0300

    Adding initial tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,10 +1,24 @@
-// import {
-//   assertEquals,
-//   assertNotEquals,
-// } from 'https://deno.land/std@0.224.0/assert/mod.ts'
-// import { memoize, compareValues } from '../src/pipe.ts'
+import { assertEquals } from 'https://deno.land/std@0.224.0/assert/mod.ts';
 
-// Deno.test('Comparing values', () => {
-//     // ...
-//   },
-// )
+import { compose } from '../src/compose.ts';
+import { pipe } from '../src/pipe.ts';
+
+Deno.test('pipe()', () => {
+  const f = pipe(
+    (n: number) => n + 1,
+    (n: number) => n ** 2,
+    (n: number) => n - 10,
+  )
+
+  assertEquals(f(5), (5 + 1) ** 2 - 10)
+})
+
+Deno.test('compose()', () => {
+  const g = compose(
+    (n: number) => n - 10,
+    (n: number) => n ** 2,
+    (n: number) => n + 1,
+  )
+
+  assertEquals(g(5), (5 + 1) ** 2 - 10)
+})
